c27dfd22ca432409710042e2c080455e80b495d9
sav1209 <saul9084@gmail.com>
2024-11-18 20:29:55 -0600

    Delete Entidad.java

--- Entidad.java
+++
@@ -1,8 +0,0 @@
-public interface Entidad {
-    // Para usar esta fábrica de objetos, cada clase debe implementar estos métodos
-    Object[] getColumns();
-    String getId();
-    String getPrnHdrs();
-    void print();
-    void update(Object[] newValues );
-}
